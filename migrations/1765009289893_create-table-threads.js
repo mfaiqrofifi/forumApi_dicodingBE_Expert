faecb5971dac38b306f72594ef936d320438532b
mfaiqrofifi <mfaiqrofifi@gmail.com>
2025-12-06 15:30:46 +0700

    First commit

---
+++ migrations/1765009289893_create-table-threads.js
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"users"',
+      onDelete: 'CASCADE',
+    },
+    date: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('CURRENT_TIMESTAMP'),
+    },
+  });
+};
+
+exports.down = pgm => {
+  pgm.dropTable('threads');
+};
